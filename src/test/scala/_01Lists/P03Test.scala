253a5216d69a5e0382b1e51c1ba6b08d7a80965a
Yunsoo <yunsoo3042@gmail.com>
2018-12-26 17:36:40 +0900

    Add P03 Test code

--- src/test/scala/_01Lists/P03Test.scala
+++ src/test/scala/_01Lists/P03Test.scala
@@ -1,5 +1,17 @@
 package _01Lists
 
-class P03Test {
+import org.scalatest.{FunSpec, Matchers}
 
+class P03Test extends FunSpec with Matchers {
+  it("should be first element when k is 0"){
+    P03.kth(List(1, 2, 3, 4, 5), 0) should be (1)
+  }
+
+  it("should be last element when k is equal to size of list minus one") {
+    P03.kth(List(1, 2, 3, 4, 5), 4) should be(5)
+  }
+
+  it("should be (k+1)th element") {
+    P03.kth(List(1, 2, 3, 4, 5), 2) should be(3)
+  }
 }
