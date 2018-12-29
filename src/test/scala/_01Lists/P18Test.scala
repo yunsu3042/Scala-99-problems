09d5d086aa86a94df4a46cf5fc5532b043b787f2
Yunsoo <yunsoo3042@gmail.com>
2018-12-29 11:21:00 +0900

    Add P11, P18, P19 Test

--- src/test/scala/_01Lists/P18Test.scala
+++ src/test/scala/_01Lists/P18Test.scala
@@ -1,5 +1,11 @@
 package _01Lists
 
-class P18Test {
+import org.scalatest.{FunSpec, Matchers}
 
+class P18Test extends FunSpec with Matchers {
+  it("should return list between two indexes") {
+    val result = P18.slice(List("a", "b", "c", "d", "e", "f", "g", "h", "i", "k"), 3, 7)
+    result should have length 5
+    result should be(List("c", "d", "e", "f", "g"))
+  }
 }
